6fe12b13519ba9028a6a4e38d87d1257b4f8f3b1
moving-line <movinglinecheck@gmail.com>
2020-02-01 10:31:28 +0900

    Add authentication
    - JwtAuthenticationFilter를 활용하여 jwt를 분석하고 인가하는 기능 기초 설정

--- eatgo-common/src/main/java/kr/co/fastcampus/eatgo/utils/JwtUtil.java
+++ eatgo-common/src/main/java/kr/co/fastcampus/eatgo/utils/JwtUtil.java
@@ -1,5 +1,6 @@
 package kr.co.fastcampus.eatgo.utils;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -14,12 +15,17 @@ public class JwtUtil {
     }
 
     public String createToken(long userId, String name) {
-        String token = Jwts.builder()
+        return Jwts.builder()
                 .claim("userId", userId)
                 .claim("name", name)
                 .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
+    }
 
-        return token;
+    public Claims getClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(key)
+                .parseClaimsJws(token)
+                .getBody();
     }
 }
