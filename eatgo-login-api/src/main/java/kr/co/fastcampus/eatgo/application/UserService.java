4f6ebc131d93c98a3a435fe4ffc2ca739b15fc3e
moving-line <movinglinecheck@gmail.com>
2020-02-03 09:59:32 +0900

    Add login module
    - 로그인 관련 기능을 처리하는 모듈 추가

--- eatgo-login-api/src/main/java/kr/co/fastcampus/eatgo/application/UserService.java
+++ eatgo-login-api/src/main/java/kr/co/fastcampus/eatgo/application/UserService.java
@@ -1,6 +1,5 @@
 package kr.co.fastcampus.eatgo.application;
 
-import kr.co.fastcampus.eatgo.EmailExistedException;
 import kr.co.fastcampus.eatgo.domain.User;
 import kr.co.fastcampus.eatgo.domain.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +7,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.Optional;
 
 @Service
 @Transactional
@@ -24,21 +22,14 @@ public class UserService {
         this.passwordEncoder = passwordEncoder;
     }
 
-    public User registerUser(String email, String name, String password) {
-        Optional<User> existed = userRepository.findByEmail(email);
-        if (existed.isPresent()) {
-            throw new EmailExistedException(email);
+    public User authenticate(String email, String password) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new EmailNotExistedException(email));
+
+        if(!passwordEncoder.matches(password, user.getPassword())) {
+            throw new PasswordWrongException();
         }
 
-        String encodedPassword = passwordEncoder.encode(password);
-
-        User user = User.builder()
-                .email(email)
-                .name(name)
-                .password(encodedPassword)
-                .level(1L)
-                .build();
-
-        return userRepository.save(user);
+        return user;
     }
 }
